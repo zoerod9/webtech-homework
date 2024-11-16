9b30a5ef03c4bf920a7f1efecf69655fbb3a7219
zoerod9 <zoe.rod9767@gmail.com>
2024-11-15 20:29:21 -0600

    added and finished HW15

---
+++ HW15/work.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div align="center" colspan="5" bgcolor="c0e4f5"><h2>Work Info</h2></div>
+
+<div class="box">
+	<h2>Welcome to Work Info</h2>
+		<div><p>I currently work for a <i>health screening company</i> as a <b>Rescreen coordinator</b><br>
+        I get to <i>work from home</i> so I quite <b>enjoy it</b><br>
+        I would like to work for a <b>medical</b> company in their tech department, I think I would like to do web development, it seems quite <i>interesting</i> to me now. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates nobis voluptate ducimus, inventore dolores est placeat quae nemo corporis ab error ullam numquam beatae dolorem fugiat earum? Consequatur ratione, autem ab laborum asperiores ex. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus adipisci dignissimos expedita voluptatum, blanditiis eos facilis non, dolores atque distinctio, debitis repudiandae facere laboriosam totam ipsa sequi, tenetur! Quasi delectus eius, consequatur neque ex blanditiis eos cumque fuga reprehenderit sunt qui maxime harum corporis rerum praesentium sapiente error veritatis incidunt.</p></div></div>
+	
+<div class="jumbotron">
+	<div class="container">
+  <h1>Surprise Jumbotron!</h1>
+  <p>Just to try it out. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates nobis voluptate ducimus, inventore dolores est placeat quae nemo corporis ab error ullam numquam beatae dolorem fugiat earum? Consequatur ratione, autem ab laborum asperiores ex. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus adipisci dignissimos expedita voluptatum, blanditiis eos facilis non, dolores atque distinctio, debitis repudiandae facere laboriosam </p>
+</div>
+	</div>
+</body>
+</html>
